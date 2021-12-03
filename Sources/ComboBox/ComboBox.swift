511b97634121518cf014a10f2a93e03b583a5f12
Bryce Campbell <tonyhawk2100@gmail.com>
2021-12-03 08:43:08 -0800

    start work on trying to make a UI.

--- Sources/ComboBox/ComboBox.swift
+++ Sources/ComboBox/ComboBox.swift
@@ -1,6 +1,17 @@
 import SwiftUI
 
-public struct ComboBox: View {
+struct ComboBox: View {
+    @State private var query = ""
     @Binding var choices: [String]
     
+    var body: some View {
+        TextField("Search", text: $query)
+    }
+}
+
+
+struct ComboBoxPreview: PreviewProvider {
+    static var previews: some View {
+        ComboBox(choices: .constant(["Hello", "World", "7"]))
+    }
 }
