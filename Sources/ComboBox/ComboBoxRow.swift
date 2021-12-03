e85880098933d926ab22b567467bacd70f05385a
Bryce Campbell <tonyhawk2100@gmail.com>
2021-12-03 10:04:52 -0800

    make custom row view.

--- Sources/ComboBox/ComboBoxRow.swift
+++ Sources/ComboBox/ComboBoxRow.swift
@@ -8,13 +8,27 @@
 import SwiftUI
 
 struct ComboBoxRow: View {
+    var title: String
+    @State var selection: String
+    var action: ()-> Void
+    
+    var isSelected: Bool {
+        return selection.caseInsensitiveCompare(title) == .orderedSame
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Button(title, action: self.action)
+            if isSelected {
+                Spacer()
+                Image(systemName: "checkmark").foregroundColor(.blue)
+            }
+        }
     }
 }
 
 struct SwiftUIView_Previews: PreviewProvider {
     static var previews: some View {
-        ComboBoxRow()
+        ComboBoxRow(title: "Hello", selection: "Hello", action: {})
     }
 }
