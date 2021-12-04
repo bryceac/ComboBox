25b2cbc4563f57cf4a6632b3a70ff42158bd1372
Bryce Campbell <tonyhawk2100@gmail.com>
2021-12-04 09:09:55 -0800

    try to make the base.

--- Sources/ComboBox/ComboBox.swift
+++ Sources/ComboBox/ComboBox.swift
@@ -8,13 +8,20 @@
 import SwiftUI
 
 struct ComboBox: View {
+    @State var choices: [String]
+    @State private var selectedItem: String = "Selection"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            NavigationLink(destination: ComboBoxList(choices: choices)) {
+                Text(selectedItem)
+            }
+        }
     }
 }
 
 struct ComboBox_Previews: PreviewProvider {
     static var previews: some View {
-        ComboBox()
+        ComboBox(choices: ["Hello", "World", "7"])
     }
 }
