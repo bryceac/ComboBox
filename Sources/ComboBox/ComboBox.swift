cf997f2ab11ff31b3000da39346ce7461cd1a785
Bryce Campbell <tonyhawk2100@gmail.com>
2021-12-04 08:58:30 -0800

    add main view for UI.

---
+++ Sources/ComboBox/ComboBox.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Bryce Campbell on 12/4/21.
+//
+
+import SwiftUI
+
+struct ComboBox: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        ComboBox()
+    }
+}
