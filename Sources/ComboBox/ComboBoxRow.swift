5a9339f31f3bda9b7706fcce60d1498bdb88ef1c
Bryce Campbell <tonyhawk2100@gmail.com>
2021-12-05 09:54:05 -0800

    add more comments.

--- Sources/ComboBox/ComboBoxRow.swift
+++ Sources/ComboBox/ComboBoxRow.swift
@@ -7,11 +7,19 @@
 
 import SwiftUI
 
+/// view that represents a row in a ComboBox
 public struct ComboBoxRow: View {
+    
+    /// the view's textual value
     var title: String
+    
+    /// the selected item in the list
     @Binding var selection: String
+    
+    /// a closure that will run when a button is clicked
     var action: ()-> Void
     
+    /// computed property to determine if a given item was selected.
     var isSelected: Bool {
         return selection.caseInsensitiveCompare(title) == .orderedSame
     }
