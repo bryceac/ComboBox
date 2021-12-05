9906bb5c25fa61040a977dae12f8d2a1b784adb2
Bryce Campbell <tonyhawk2100@gmail.com>
2021-12-05 13:26:29 -0800

    try implementing code for macOS.

--- Sources/ComboBox/macOS/macComboBox.swift
+++ Sources/ComboBox/macOS/macComboBox.swift
@@ -6,9 +6,11 @@
 //
 
 import SwiftUI
-import Appkit
+import AppKit
 
 struct macComboBox: NSViewRepresentable {
+    typealias NSViewType = NSComboBox
+    
     @Binding var content: [String]
     var numberOfVisibleItems: Int
     @State var selectedIndex: Int
@@ -17,13 +19,41 @@ struct macComboBox: NSViewRepresentable {
         return content[selectedIndex]
     }
     
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    final class Coordinator: NSObject, NSComboBoxDelegate {
+        
+        @Binding var selected: Int
+        
+        init(selected: Binding<Int>) {
+            self._selected = selected
+        }
+        
+        func comboBoxSelectionDidChange(_ notification: Notification) {
+            guard let combo = notification.object as? NSComboBox, selected != combo.indexOfSelectedItem else { return }
+            
+            selected = combo.indexOfSelectedItem
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(selected: $selectedIndex)
+    }
+    
+    func makeNSView(context: Context) -> NSComboBox {
+        let combo = NSComboBox()
+        combo.numberOfVisibleItems = numberOfVisibleItems
+        combo.indexOfSelectedItem = selectedIndex
+        combo.usesDataSource = false
+        
+        return combo
+    }
+    
+    func updateNSView(_ nsView: NSComboBox, context: Context) {
+        <#code#>
     }
 }
 
 struct SwiftUIView_Previews: PreviewProvider {
     static var previews: some View {
-        macComboBox()
+        macComboBox(content: .constant(["Hello", "World", "7"]), numberOfVisibleItems: 5, selectedIndex: 0)
     }
 }
