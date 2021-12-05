95670b5db553bd36c2f90131af2c57146e25554b
Bryce Campbell <tonyhawk2100@gmail.com>
2021-12-05 10:57:47 -0800

    work on Mac view.

--- Sources/ComboBox/macOS/macComboBox.swift
+++ Sources/ComboBox/macOS/macComboBox.swift
@@ -9,13 +9,53 @@ import SwiftUI
 import AppKit
 
 struct macComboBox: NSViewRepresentable {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    typealias NSViewType = NSComboBox
+    
+    var content: [String]
+    var numberOfVisibleItems: Int
+    
+    @Binding var selected: String
+    
+    final class Coordinator: NSObject, NSComboBoxDelegate {
+        var selected: Binding<String>
+        
+        init(selected: Binding<String>) {
+            self.selected = selected
+        }
+        
+        func comboBoxSelectionDidChange(_ notification: Notification) {
+            guard let combo = notification.object as? NSComboBox, selected.wrappedValue != combo[combo.indexOfSelectedItem] else { return }
+            
+            selected.wrappedValue = combo[combo.indexOfSelectedItem]
+        }
     }
+    
+    func makeCoordinator() -> macComboBox.Coordinator {
+        Coordinator(selected: $selected)
+    }
+    
+    func makeNSView(context: NSViewRepresentableContext<macComboBox>) -> NSComboBox {
+        let comboBox = NSComboBox()
+        comboBox.numberOfVisibleItems = numberOfVisibleItems
+        comboBox.usesDataSource = false
+        comboBox.delegate = context.coordinator
+        
+        for choice in content {
+            comboBox.addItem(withObjectValue: choice)
+        }
+        
+        return comboBox
+    }
+    
+    func updateNSView(_ nsView: NSComboBox, context: Context) {
+        <#code#>
+    }
+    
+    
 }
 
 struct macComboBox_Previews: PreviewProvider {
     static var previews: some View {
-        macComBox()
+        macComboBox()
     }
 }
